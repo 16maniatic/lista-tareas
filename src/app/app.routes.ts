d478931044c12a531ac383ee93793329f587daf3
Vicente García <centeclan15@gmail.com>
2025-02-24 10:07:19 +0100

    Ejercicio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { ListadoTareasComponent } from './listado-tareas/listado-tareas.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: InicioComponent },
+  { path: 'listado-tareas', component: ListadoTareasComponent }
+];
